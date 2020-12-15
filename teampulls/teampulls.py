19e8e4125d1bc378f17cdcbee809e2eee311997a
Jochen Breuer <jbreuer@suse.de>
2020-12-15 10:55:35 +0100

    Reversing the order of source and target branch
    With the target branch first, it's easier to find this more
    important branch. Also it's the same order Github uses.

--- teampulls/teampulls.py
+++ teampulls/teampulls.py
@@ -198,7 +198,7 @@ def print_prs_detail(data, repos):
         source_branch = pr["headRefName"]
         print("{}{}{}".format(get_colour_coding_for_pr(pr), title, bcolors.ENDC))
         print("🔗 {}".format(url))
-        print("🌿 {} ➡ {}{}{}".format(source_branch, bcolors.BOLD, branch, bcolors.ENDC))
+        print("🌿 {} ⬅ {}".format(branch, source_branch))
         if i + 1 == len(pull_requests):
             print("\n")
             continue
